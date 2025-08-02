88b10dd06b4e9869959dbc9944fd3cbe9bc1ae37
nouralhuda2025 <archerh991@gmail.com>
2025-08-02 14:48:10 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
+
+import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = "8486150937:AAEZAmbMhXkONZDb5wKojZy0jCr8EgCP0qk"
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
+# أمر /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("أهلين فيك! كيف فيني أساعدك؟")
+    await update.message.reply_text("أهلًا وسهلًا في بوت نور الهدى 🌸 كيف بقدر أساعدك؟")
 
 if __name__ == '__main__':
+    import os
+    TOKEN = os.environ.get("BOT_TOKEN")
+
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
+
     app.run_polling()
